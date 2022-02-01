18be1d4a5efd36c6faa344be6dc2e0cb26638564
muklasar <muklasar@gmail.com>
2022-02-01 11:43:37 +0530

    focus-on-input

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const Search = ({ data, setShips }) => {
     const [text, setText] = useState()
     const [update, setUpdate] = useState()
+    const iElement = useRef()
 
     const searchFilter = () => data && data.filter(dt=>dt.name.toLowerCase().match(text.toLowerCase()))
 
@@ -11,9 +12,13 @@ const Search = ({ data, setShips }) => {
         // setText('')
     },[update])
 
+    useEffect(()=>{
+        iElement.current.focus()
+    },[])
+
     return (
         <div className='search__section'>
-            <input type="text" className='search__input' onChange={(e)=>setText(e.target.value)} value={text} placeholder="Search ships"/>
+            <input type="text" className='search__input' onChange={(e)=>setText(e.target.value)} value={text} placeholder="Search ships" ref={iElement}/>
             <button className='search__button' onClick={()=>setUpdate(!update)}>
                 <img className='search__logo' src="https://firebasestorage.googleapis.com/v0/b/mystore-51bd6.appspot.com/o/image%2Fmagnifying-glass.png?alt=media&token=16a5f0d8-ece6-4354-b2d5-09d547cfd012" />
             </button>
